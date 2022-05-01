03e1443d9c26e03f9f50e7b1c59f4dcf2f9a9eb4
Kelvin-Air1234 <kelvinairerayanmen@gmail.com>
2022-05-01 16:28:26 +0100

    Commit

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 import requests
+from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from . import models
+
+BASE_URL = 'https://stockx.com/search?s={}'
+Another_URL = 'https://www.icebox.com/search-products?keyword={}'
 
 # Create your views here.
 def home(request):
@@ -8,6 +14,13 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    print(quote_plus(search))
+    final_url = BASE_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get('https://stockx.com/nike-air-force-1-low-supreme-box-logo-black')
+    data = response.text
+    # print(data)
     stuff_for_frontend = {
         'search': search
     }
